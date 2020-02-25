98df442a9951cc601679909a25a01f428d0b9389
Yoann Fleury <yoann.fleury@saagie.com>
2020-02-25 11:11:09 +0100

    ✅ add output util tests
    update test dependencies and change folder name

---
+++ packages/sdk/src/cli/utils/__tests__/output.test.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+const output = require('../output');
+
+let loggedData;
+const storeLog = (inputs) => { loggedData += inputs; };
+const consoleLog = console.log;
+
+beforeEach(() => {
+  loggedData = '';
+  console.log = jest.fn(storeLog);
+});
+
+afterEach(() => {
+  console.log = consoleLog;
+});
+
+describe('each output is successful using', () => {
+  test('an error message', () => {
+    const MESSAGE = 'fatal: this is an error message';
+
+    output.error(MESSAGE);
+    expect(loggedData).toBe(MESSAGE);
+  });
+
+  test('a success message', () => {
+    const MESSAGE = 'this is a success message';
+
+    output.success(MESSAGE);
+    expect(loggedData).toBe(MESSAGE);
+  });
+
+  test('an info message', () => {
+    const MESSAGE = 'this is an info message';
+
+    output.info(MESSAGE);
+    expect(loggedData).toBe(MESSAGE);
+  });
+
+  test('a warning message', () => {
+    const MESSAGE = 'this is a warning message';
+
+    output.info(MESSAGE);
+    expect(loggedData).toBe(MESSAGE);
+  });
+});
