19091d1db443409edbee456389378cc3897f8e18
Ivan Dalmet <ivan@dalmet.fr>
2020-03-16 18:57:11 +0100

    ✨: Improve build command

--- packages/sdk/src/cli/commands/build.js
+++ packages/sdk/src/cli/commands/build.js
@@ -1,5 +1,7 @@
+const figures = require('figures');
+const chalk = require('chalk');
 const { ERROR_CODE, BUILD_FOLDER, TECHNOLOGY } = require('../constants');
-const { error, info } = require('../utils/output');
+const output = require('../utils/output');
 const isRoot = require('../validators/isRoot');
 const isFileEmpty = require('../validators/isFileEmpty');
 const isYAMLFileValid = require('../validators/isYAMLFileValid');
@@ -9,24 +11,29 @@ const { buildJS } = require('../utils/bundler');
 module.exports = async () => {
   // Check if the user is in a technology folder.
   if (!await isRoot()) {
-    error('fatal: not a technology folder');
+    output.error('fatal: not a technology folder');
     process.exit(ERROR_CODE.NOT_A_TECHNOLOGY_FOLDER);
   }
 
   // Make sure that technology yaml file is not empty.
   if (await isFileEmpty(TECHNOLOGY.FILENAME_GLOB)) {
-    error('fatal: technology yaml file is empty');
+    output.error('fatal: technology yaml file is empty');
     process.exit(ERROR_CODE.TECHNOLOGY_YAML_IS_EMPTY);
   }
 
   // Make sure that technology yaml file is valid.
   if (!await isYAMLFileValid(TECHNOLOGY.FILENAME_GLOB)) {
-    error('fatal: technology yaml file is not valid');
+    output.error('fatal: technology yaml file is not valid');
     process.exit(ERROR_CODE.TECHNOLOGY_YAML_IS_NOT_VALID);
   }
 
-  await generateMetadataFile();
-  await buildJS();
+  output.log(chalk`\n🚀 {bold Building technology}`);
 
-  info(`Build done in ${BUILD_FOLDER} folder`);
+  await generateMetadataFile();
+  output.log(`${figures.tick} Metadata.yaml generated`);
+
+  await buildJS();
+  output.log(`${figures.tick} JS files bundled`);
+
+  output.success(`\nBuild done in ${BUILD_FOLDER} folder`);
 };
--- packages/sdk/src/cli/utils/output.js
+++ packages/sdk/src/cli/utils/output.js
@@ -4,5 +4,5 @@ const chalk = require('chalk');
 exports.success = (output) => console.log(chalk.bold.green(output));
 exports.warning = (output) => console.log(chalk.yellow(output));
 exports.error = (output) => console.log(chalk.bold.red(output));
-exports.info = (output) => console.log(chalk.blue(output));
+exports.info = (output) => console.log(chalk.cyan(output));
 exports.log = (output) => console.log(output);
