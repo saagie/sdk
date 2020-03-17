3fbdd776f1968709caabaf8762647122936f4105
Yoann Fleury <yoann.fleury@saagie.com>
2020-03-17 18:07:40 +0100

    ✨ check for minimum node engine in cli

--- packages/sdk/src/cli/cli.js
+++ packages/sdk/src/cli/cli.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const semver = require('semver');
+const figures = require('figures');
 
 const init = require('./commands/init');
 const start = require('./commands/start');
 const build = require('./commands/build');
-const { error } = require('./utils/output');
+const output = require('./utils/output');
 const { ERROR_CODE } = require('./constants');
 
-const { version } = require('../../package.json');
+const { version, engines } = require('../../package.json');
+
+if (!semver.satisfies(process.version, engines.node)) {
+  output.error(`${figures.warning} Your node version ${process.version} is not supported, please use ${engines.node}`);
+
+  process.exit(ERROR_CODE.NODE_VERSION_NOT_SUPPORTED);
+}
 
 program.version(version);
 
@@ -27,7 +35,7 @@ program.command('build')
 
 // Output error and help on unknown command.
 program.on('command:*', () => {
-  error(`invalid command: ${program.args.join(' ')}`);
+  output.error(`invalid command: ${program.args.join(' ')}`);
   program.outputHelp();
   process.exit(ERROR_CODE.INVALID_COMMAND);
 });
