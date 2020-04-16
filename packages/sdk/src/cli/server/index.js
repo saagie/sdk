4052a0a3ef98b55f38d91c5788d854188f52e4ed
Yoann Fleury <yoann.fleury@saagie.com>
2020-04-16 12:11:30 +0200

    ✨ show the versions in the UI (#39)

--- packages/sdk/src/cli/server/index.js
+++ packages/sdk/src/cli/server/index.js
@@ -6,8 +6,10 @@ const path = require('path');
 const { DEFAULT_PORT } = require('../constants');
 const action = require('./services/action');
 const config = require('./services/config');
+const info = require('./services/info');
 const serveFiles = require('./services/static');
 const demo = require('./services/demo');
+const { version } = require('../../../package.json');
 
 module.exports = ({ port = DEFAULT_PORT } = {}) => {
   const server = express();
@@ -24,6 +26,7 @@ module.exports = ({ port = DEFAULT_PORT } = {}) => {
 
   server.get('/api/config', config);
   server.post('/api/action', action);
+  server.get('/api/info', info({ port, version }));
   server.get('/api/static', serveFiles);
 
   server.use('/api/demo', demo);
