ed0bc74ba5e8806b2c904cf9893e4bb05f931f43
Yoann Fleury <yoann.fleury@saagie.com>
2020-05-28 11:02:13 +0200

    ⚡ huge performance improvement on js transpilation (#66)

--- packages/sdk/src/cli/utils/bundler.js
+++ packages/sdk/src/cli/utils/bundler.js
@@ -1,27 +1,20 @@
+const path = require('path');
 const globby = require('globby');
-const Parcel = require('parcel-bundler');
+const { build } = require('esbuild');
 const { BUILD_FOLDER } = require('../constants');
 const { error } = require('./output');
 
 exports.buildJS = async () => {
   const paths = await globby(['./**/*.js', `!${BUILD_FOLDER}`, '!node_modules']);
 
-  const parcel = new Parcel(
-    paths,
-    {
-      outDir: BUILD_FOLDER,
-      minify: true,
-      watch: false,
-      target: 'node',
-      bundleNodeModules: true,
-      sourceMaps: false,
-      autoInstall: false,
-      logLevel: 2,
-    },
-  );
-
   try {
-    await parcel.bundle();
+    paths.forEach(async (jsPath) => build({
+      entryPoints: [jsPath],
+      outfile: path.resolve(BUILD_FOLDER, jsPath),
+      platform: 'node',
+      minify: true,
+      bundle: true,
+    }));
   } catch (err) {
     error(err);
   }
