8e0f89d516a14150c7373b4b695d0bae48ab33f9
Yoann Fleury <yoann.fleury@saagie.com>
2020-05-29 15:09:41 +0200

    🐛 rollback to parcel for build
    esbuild available through --bundler command line option

--- packages/sdk/src/cli/server/services/action.js
+++ packages/sdk/src/cli/server/services/action.js
@@ -1,44 +1,9 @@
 const fse = require('fs-extra');
 const path = require('path');
-const Parcel = require('parcel-bundler');
-const { build } = require('esbuild');
 const { Response } = require('../../../sdk');
-
 const output = require('../../utils/output');
+const { ENGINES } = require('../../utils/engines');
 const { BUNDLE_FOLDER, BUNDLERS } = require('../../constants');
-
-const ENGINES = {
-  parcel: async (scriptPath, outFile) => {
-    const parcel = new Parcel(
-      scriptPath,
-      {
-        outDir: path.resolve(process.cwd(), BUNDLE_FOLDER),
-        outFile,
-        minify: true,
-        watch: false,
-        target: 'node',
-        bundleNodeModules: true,
-        sourceMaps: false,
-        autoInstall: false,
-      },
-    );
-
-    const bundle = await parcel.bundle();
-    return bundle.name;
-  },
-  esbuild: async (scriptPath, outfile) => {
-    await build({
-      entryPoints: [scriptPath],
-      outfile,
-      platform: 'node',
-      minify: true,
-      bundle: true,
-      sourcemap: true,
-    });
-
-    return outfile;
-  },
-};
 
 module.exports = async (req, res) => {
   try {
