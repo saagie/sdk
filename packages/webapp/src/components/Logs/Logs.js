54374c790fde029b5ba886544235b33c491065c6
Yoann Fleury <yoann.fleury@saagie.com>
2020-07-17 17:15:57 +0200

    🐛 logs component is more stable
    For the record, the previous logs component was printing lines on
    multiple lines, and a change was made so a horizontal scrollbar
    appeared on bottom so lines were on one line.
    
    The computation was not removed and was causing a bug using the
    vertical scrollbar or while scrolling into the view.
    
    As part of this commit, the computation is removed, allowing a more
    stable and a more performant component.
    
    closes #88

--- packages/webapp/src/components/Logs/Logs.js
+++ packages/webapp/src/components/Logs/Logs.js
@@ -1,10 +1,8 @@
-import React, { useRef, useCallback } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { VariableSizeList as List } from 'react-window';
+import { FixedSizeList as List } from 'react-window';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import { EmptyState } from 'saagie-ui/react';
-import { useWindowSize } from '../../hooks/useWindowSize';
-import { LogsContext } from '../../contexts/LogsContext';
 import { Line } from './Line';
 
 const propTypes = {
@@ -20,44 +18,34 @@ const defaultProps = {
 };
 
 export const Logs = ({ logs }) => {
-  const listRef = useRef();
-  const sizeMap = useRef({});
-  const setSize = useCallback((index, size) => {
-    sizeMap.current = { ...sizeMap.current, [index]: size };
-    listRef.current.resetAfterIndex(index);
-  }, []);
-  const getSize = useCallback((index) => sizeMap.current[index] || 20, []);
-  const [windowWidth] = useWindowSize();
-
   if (!logs || logs.length <= 0) {
     return (
       <EmptyState icon="logs" content="No logs available" />
     );
   }
 
+  const ITEM_SIZE = 19;
+
   return (
-    <LogsContext.Provider value={{ setSize, windowWidth }}>
-      <div className="sdk-a-logs">
-        <AutoSizer>
-          {({ height, width }) =>
-            (
-              <List
-                height={height}
-                itemCount={logs.length}
-                itemSize={getSize}
-                width={width}
-                ref={listRef}
-              >
-                {({ index, style }) => (
-                  <div style={style}>
-                    <Line index={index} line={logs[index]} />
-                  </div>
-                )}
-              </List>
-            )}
-        </AutoSizer>
-      </div>
-    </LogsContext.Provider>
+    <div className="sdk-a-logs">
+      <AutoSizer>
+        {({ height, width }) =>
+          (
+            <List
+              height={height}
+              itemCount={logs.length}
+              itemSize={ITEM_SIZE}
+              width={width}
+            >
+              {({ index, style }) => (
+                <div style={style}>
+                  <Line index={index} line={logs[index]} />
+                </div>
+              )}
+            </List>
+          )}
+      </AutoSizer>
+    </div>
   );
 };
 
--- packages/webapp/src/contexts/LogsContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const LogsContext = createContext({});
-export const useLogsContext = () => useContext(LogsContext);
