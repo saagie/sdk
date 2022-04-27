445425367b0b82d3616c774f4c96f1db65816b3f
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2022-04-27 21:24:51 +0200

    Run the script in a forked process

--- packages/sdk/src/cli/server/services/action.js
+++ packages/sdk/src/cli/server/services/action.js
@@ -1,8 +1,7 @@
 const fse = require('fs-extra');
 const path = require('path');
-const { Stream } = require('stream');
-const output = require('../../utils/output');
-const { stringify, runScript } = require('../../utils/runScript');
+const { runScript } = require('../../utils/script-runner');
+const { stringify } = require('../../utils/script-utils');
 
 const Response = {
   success: (data, logs) => ({
@@ -15,35 +14,40 @@ const Response = {
   }),
 };
 
-function onError(error, res, logs) {
-  output.error(error);
-  res.status(420).send(Response.error(stringify(error), logs));
-}
-
 module.exports = async (req, res) => {
   const logs = [];
+  let result = null;
+  const scriptPath = path.resolve(process.cwd(), req.body.script);
   try {
-    const scriptPath = path.resolve(process.cwd(), req.body.script);
-
     if (!await fse.pathExists(scriptPath || '')) {
       const message = `Unable to find file ${scriptPath}, please check the path in context.yaml`;
-      output.error(message);
-      res.status(421).send(Response.error({ message }));
+      res.status(421)
+        .send(Response.error({ message }));
 
       return;
     }
 
-    const logger = (log) => logs.push(log);
     const scriptData = fse.readFileSync(scriptPath);
-    const data = await runScript(scriptData, req.body.function, req.body.params, logger);
-    if (data instanceof Stream) {
-      data.pause();
-      data.on('error', (err) => onError(err, res, logs));
-      data.pipe(res);
-    } else {
-      res.send(Response.success(data, logs));
+
+    const scriptLogger = (name, log) => {
+      log.split(/\r\n|\n\r|\n|\r/)
+        .forEach((logPart) => {
+          logs.push({
+            name,
+            message: logPart,
+          });
+        });
+    };
+    // eslint-disable-next-line max-len
+    result = await runScript(scriptPath, scriptData.toString(), req.body.function, [req.body.params], scriptLogger, res);
+  } catch (e) {
+    if (e == null) {
+      return;
     }
-  } catch (err) {
-    onError(err, res, logs);
+    res.status(420).send(Response.error(stringify(e), logs));
+  }
+  if (result != null) {
+    res.set('Content-Type', 'application/json');
+    res.send(Response.success(result.content, logs));
   }
 };
---
+++ packages/sdk/src/cli/utils/script-runner.js
@@ -0,0 +1,115 @@
+// eslint-disable-next-line max-classes-per-file
+const { fork } = require('child_process');
+const path = require('path');
+
+class ScriptExecError extends Error {
+  constructor(scriptId, fun, name, message, stack) {
+    super(message);
+    this.scriptId = scriptId;
+    this.fun = fun;
+    this.name = name;
+    this.message = message;
+    this.stack = stack;
+  }
+}
+exports.ScriptExecError = ScriptExecError;
+
+const childProcessTimeout = 10000; // FIXME: to be retrieve from conf
+
+// eslint-disable-next-line max-len
+exports.runScript = async (scriptId, scriptData, fun, args, scriptLogger, stream) => new Promise((resolve, reject) => {
+  // https://github.com/patriksimek/vm2/issues/355
+  // this function can safely be used from the main process. sandbox is limited to JSON values
+  const childRunnerFile = path.resolve(__dirname, 'script-runner-child');
+  const childProcess = fork(childRunnerFile, [], {
+    env: {},
+    execArgv: [],
+  });
+  let lastStreamedChunkDate = null;
+
+  function setupKillTimeout(timeout) {
+    return setTimeout(() => {
+      if (lastStreamedChunkDate == null) {
+        childProcess.kill(9);
+      } else {
+        const now = new Date();
+        const timeWithoutChunk = now.getTime() - lastStreamedChunkDate.getTime();
+        if (timeWithoutChunk > timeout) {
+          childProcess.kill(9);
+        } else {
+          // eslint-disable-next-line no-use-before-define
+          kto = setupKillTimeout(childProcessTimeout - timeWithoutChunk);
+        }
+      }
+    }, timeout);
+  }
+
+  let kto = setupKillTimeout(childProcessTimeout);
+  childProcess.on('message', (m) => {
+    if (m.type === 'result') {
+      clearTimeout(kto);
+      resolve({ content: JSON.parse(m.result) });
+      childProcess.kill();
+    } else if (m.type === 'chunk') {
+      childProcess.send({ ack: 1 });
+      if (lastStreamedChunkDate == null) {
+        stream.write('[');
+      } else {
+        stream.write(',');
+      }
+      lastStreamedChunkDate = new Date();
+      stream.write(JSON.stringify(m.chunk));
+    } else if (m.type === 'finished') {
+      clearTimeout(kto);
+      if (lastStreamedChunkDate != null) {
+        stream.write(']');
+        stream.end();
+      }
+      resolve(null);
+      childProcess.kill();
+    } else if (m.type === 'log') {
+      scriptLogger(m.name, m.message);
+    } else if (m.type === 'error') {
+      clearTimeout(kto);
+      if (lastStreamedChunkDate != null) {
+        stream.end();
+      }
+      reject(new ScriptExecError(scriptId, fun, m.error.name, m.error.message, m.error.stack));
+      childProcess.kill();
+    } else {
+      // eslint-disable-next-line no-console
+      console.error('Unexpected message type from child', m);
+    }
+  });
+  childProcess.on('error', (err) => {
+    clearTimeout(kto);
+    if (lastStreamedChunkDate != null) {
+      stream.end();
+    }
+    reject(new ScriptExecError(scriptId, fun, 'ScriptHaltError', err.message, null));
+  });
+  childProcess.on('disconnect', () => {
+    clearTimeout(kto);
+    if (lastStreamedChunkDate != null) {
+      stream.end();
+    }
+    reject(new ScriptExecError(scriptId, fun, 'ScriptHaltError', 'disconnected', null));
+  });
+  childProcess.on('exit', (c) => {
+    clearTimeout(kto);
+    if (lastStreamedChunkDate != null) {
+      stream.end();
+    }
+    if (c !== 0) {
+      reject(new ScriptExecError(scriptId, fun, 'ScriptHaltError', `exit code ${c}`, null));
+    }
+  });
+  childProcess.send({ scriptData, fun, args });
+});
+
+class Result {
+  constructor(content) {
+    this.content = content;
+  }
+}
+exports.Result = Result;
