778fcf5f41ef3e0fad99e6f87393a24907b94e59
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2022-05-16 16:03:15 +0200

    Display log timestamps

--- packages/webapp/src/components/Logs/Line.js
+++ packages/webapp/src/components/Logs/Line.js
@@ -5,14 +5,15 @@ import PropTypes from 'prop-types';
 const propTypes = {
   index: PropTypes.number.isRequired,
   line: PropTypes.string.isRequired,
+  timestamp: PropTypes.number.isRequired,
 };
 
-export function Line({ index, line }) {
+export function Line({ index, line, timestamp }) {
   const contentRef = useRef();
 
   return (
     <div className="sdk-a-logs__line">
-      <Tooltip defaultPlacement="left">
+      <Tooltip defaultPlacement="left" label={timestamp ? new Date(timestamp).toISOString() : 'Not available'}>
         <div className="sdk-a-logs__line-clock"><Icon name="fa-clock-o" /></div>
       </Tooltip>
       <div className="sdk-a-logs__line-index">{index + 1}</div>
