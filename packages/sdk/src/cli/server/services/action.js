9faaf3fa6caad4f6188bfce87ae40569f4792f9e
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2022-05-19 19:28:15 +0200

    Make the first lines of logs displayed and the ull content downloadable

--- packages/sdk/src/cli/server/services/action.js
+++ packages/sdk/src/cli/server/services/action.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line max-classes-per-file
 const fs = require('fs');
 const fse = require('fs-extra');
 const path = require('path');
-const { Transform } = require('stream');
+const { Transform, Readable } = require('stream');
 const { runScript } = require('../../utils/script-runner');
 const { stringify } = require('../../utils/script-utils');
 
@@ -42,6 +43,39 @@ class PayloadTransform extends Transform {
   }
 }
 
+class LogTransform extends Transform {
+  constructor() {
+    super({ readableOjectMode: false, writableObjectMode: true });
+  }
+
+  _transform(chunk, _, callback) {
+    this.push(new Date(chunk.timestamp).toISOString());
+    this.push(': ');
+    this.push(chunk.log);
+    this.push('\n');
+    callback();
+  }
+}
+
+class BufferHeadTransform extends Transform {
+  constructor(max) {
+    super({ readableOjectMode: true, writableObjectMode: true, objectMode: true });
+    this.max = max;
+    this.head = [];
+    this.hasMore = false;
+  }
+
+  _transform(chunk, _, callback) {
+    if (this.head.length < this.max) {
+      this.head.push(chunk);
+    } else {
+      this.hasMore = true;
+    }
+    this.push(chunk);
+    callback();
+  }
+}
+
 module.exports = async (req, res) => {
   const scriptStdOut = [];
   const scriptPath = path.resolve(process.cwd(), req.body.script);
@@ -71,44 +105,62 @@ module.exports = async (req, res) => {
       download = false,
     } = req.body.opts || {};
 
-    // eslint-disable-next-line max-len
-    const stream = await runScript(scriptPath, scriptData.toString(), req.body.function, [req.body.params], scriptLogger);
+    let stream = await runScript(
+      scriptPath,
+      scriptData.toString(),
+      req.body.function,
+      [req.body.params],
+      scriptLogger,
+    );
 
     res.set('Content-Type', 'application/json');
 
+    let downloadLink = null;
+    let hasMore = null;
     if (download) {
       const logpath = `${process.cwd()}/action.log`;
+      downloadLink = {
+        href: '/api/static?path=action.log',
+        name: 'action.log',
+      };
       const destination = fs.createWriteStream(logpath);
-      stream
-        .pipe(new PayloadTransform())
-        .pipe(destination)
-        .on('finish', () => {
-          res.status(200)
-            .send(Response.success(
-              [{ timestamp: new Date().getTime(), log: `Logs downloaded at ${logpath}` }],
-              scriptStdOut,
-            ));
-        });
-    } else {
-      res.write('{"payload":');
-
-      const payload = stream.pipe(new PayloadTransform());
-
-      payload.pipe(res, { end: false });
-
-      payload.on('end', () => {
-        res.write(',"logs":[');
-        scriptStdOut.forEach((log, i) => {
-          if (i > 0) {
-            res.write(',');
-          }
-          res.write(JSON.stringify(log));
-        });
-        res.write(']}');
-
-        res.end();
+      const head = new BufferHeadTransform(100);
+      await new Promise((fulfill) => {
+        stream
+          .pipe(head)
+          .pipe(new LogTransform())
+          .pipe(destination)
+          .on('finish', fulfill);
       });
+      hasMore = head.hasMore;
+      stream = Readable.from(head.head);
     }
+    res.write('{"payload":');
+
+    const payload = stream.pipe(new PayloadTransform());
+
+    payload.pipe(res, { end: false });
+
+    payload.on('end', () => {
+      res.write(',"logs":[');
+      scriptStdOut.forEach((log, i) => {
+        if (i > 0) {
+          res.write(',');
+        }
+        res.write(JSON.stringify(log));
+      });
+      res.write(']');
+      if (downloadLink !== null) {
+        res.write(',"download":');
+        res.write(JSON.stringify(downloadLink));
+      }
+      if (hasMore !== null) {
+        res.write(',"hasMore":');
+        res.write(hasMore ? 'true' : 'false');
+      }
+      res.write('}');
+      res.end();
+    });
   } catch (e) {
     if (e == null) {
       return;
